0ac680a4ad79fe9fc119d1bf75c589389bb0c383
Pranav Darekar <97031399+pranavdarekar07@users.noreply.github.com>
2025-05-18 19:35:59 +0530

    Add files via upload

---
+++ grade_calculator.py
@@ -0,0 +1,13 @@
+score = int(input("Enter you score : "))
+if 90 <= score <= 100:
+    print("Grade : A")
+elif 80 <= score <= 89:
+    print("Grade : B")
+elif 70 <= score <= 79:
+    print("Grade : C")
+elif 60 <= score <= 69:
+    print("Grade : D")
+elif 50 <= score <= 59:
+    print("Grade : E")
+else : 
+    print("Fail")
